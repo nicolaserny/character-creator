499b51a1dca0559b7ed9614e6209b299eb46e693
nicolaserny <nicolas.erny@xcomponent.com>
2021-03-28 18:12:21 +0200

    Workshop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import Background from "./components/Background";
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Background />
       <CharacterEditor />
       <Footer />
     </>
